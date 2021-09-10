034a319616739985cac7ad2e803d13c0419c8bc1
danielandersondasilva <danielandersondasilva@gmail.com>
2021-09-10 13:41:50 -0300

    aula 2 cc50 pela metade, parei no codigo da maquina de doces

---
+++ hacker1/skittles.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cc50.h>
+
+int main(void)
+{
+    //int randon = srand(time(NULL));
+    //int skittles = rand() % 1024;
+    printf("asad");
+}
