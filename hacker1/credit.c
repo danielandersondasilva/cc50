7922333e865a8b9d50dbf217db4e698bec7799f0
danielandersondasilva <danielandersondasilva@gmail.com>
2021-09-20 12:46:29 -0300

    ainda nao finalizei, tentar fazer em python

--- hacker1/credit.c
+++ hacker1/credit.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <cc50.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    char teste[20];
     printf("Digite o número do seu cartão de crédito:");
     long long cartao = GetLongLong();
     printf("O seu cartão tem esse número %lld\n", cartao);
+    sprintf(teste, "%lld", cartao);
+    // prinf("%s", teste);
+    for (size_t i = 0; i < strlen(teste); i += 2)
+    {
+        printf("%s", teste);
+    }
 
     return 0;
 }
